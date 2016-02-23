679de4268d15389f609e10c94d6c50f40444a52c
julischuster <julian.schuster@despegar.com>
2016-02-23 19:13:56 -0300

    primer commit con gitignore, ahora voy a empezar a trabajar

---
+++ src/paquete/Clientes.java
@@ -0,0 +1,37 @@
+package paquete;
+
+public class Clientes {
+	
+	private String nombre;
+	private double saldo;
+	private double impuesto;
+	
+	public String getNombre() {
+		return nombre;
+	}	
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getImpuesto() {
+		return impuesto;
+	}
+
+	public void setImpuesto(double impuesto) {
+		this.impuesto = impuesto;
+	}
+
+	
+	
+	
+
+}
